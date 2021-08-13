b65be7a2699ea641558379a9eb77ec792d5d424e
MaoLin Sun <maolin.sun@questmobile.com.cn>
2021-08-14 00:47:31 +0800

    注解进行权限校验

--- src/main/java/com/qm/security/SecurityDemoApplication.java
+++ src/main/java/com/qm/security/SecurityDemoApplication.java
@@ -3,9 +3,14 @@ package com.qm.security;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @MapperScan("com.qm.security.mapper")
+/**
+ * 开启security注解，也可以写在配置类上
+ */
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
 public class SecurityDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/qm/security/controller/IndexController.java
+++ src/main/java/com/qm/security/controller/IndexController.java
@@ -1,5 +1,10 @@
 package com.qm.security.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +26,34 @@ public class IndexController {
     @GetMapping("/admin")
     public String test() {
         return "admin";
+    }
+
+    @GetMapping("/update")
+    /**
+     * 注解 角色校验，拥有该角色用户才可访问（只能写角色）
+     * 需要开启注解
+     * @EnableGlobalMethodSecurity(securedEnabled = true)
+     */
+    //@Secured({"ROLE_boss"})
+    /**
+     * 注解 权限校验 可以写权限，也可以写角色。 进入方法之前校验
+     * hasAuthority("admins")
+     * hasAnyAuthority("admins","manager")
+     * hasRole("sale")
+     * hasAnyRole("sale", "boss")
+     * 需要开启注解
+     * @EnableGlobalMethodSecurity(prePostEnabled = true)
+     */
+    //@PreAuthorize("hasAnyAuthority('admins')")
+    /**
+     * 与上一个相似，区别在 方法之后进行校验
+     */
+    @PostAuthorize("hasAnyAuthority('admins')")
+    // 对传入，返回的数据过滤，不常用
+    //@PreFilter()
+    //@PostFilter()
+    public String update() {
+        return "hello update";
     }
 
 }
